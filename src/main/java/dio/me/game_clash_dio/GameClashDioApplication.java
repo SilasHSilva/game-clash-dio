c311a5db0779ad2e67043bbe15acabacbe668018
Silas Henrique <Silas.henrique21@outlook.com>
2025-04-03 17:44:49 -0300

    Initial commit

---
+++ src/main/java/dio/me/game_clash_dio/GameClashDioApplication.java
@@ -0,0 +1,13 @@
+package dio.me.game_clash_dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameClashDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameClashDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/me/game_clash_dio/GameClashDioApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.me.game_clash_dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameClashDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
